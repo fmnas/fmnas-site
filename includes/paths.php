47cad22ade3fbe45ce3b1e8b45b35a78174739c8
Sean Gillen <sean@forgetmenotshelter.org>
2017-10-18 17:43:05 -0500

    blueimp stuff

--- includes/paths.php
+++ includes/paths.php
@@ -4,20 +4,4 @@
 
 	$BASE = $_SERVER['DOCUMENT_ROOT'].'/'.$document_root.'/';
 
-	//Runtime dependencies
-
-		//Client-side scripts
-
-			//jQuery
-			$jquery_path = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
-
-			//TinyMCE
-			$tinymce_path = 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.1/tinymce.min.js';
-			$tinymce_jquery_path = 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.1/jquery.tinymce.min.js';
-			$tinymce_plugins = array();
-			$tinymce_plugins[] = 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.1/plugins/lists/plugin.min.js';
-			$tinymce_plugins[] = 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.1/plugins/link/plugin.min.js';
-			$tinymce_plugins[] = "/".$document_root."tinymce_plugins/pets/plugin.js";
-			$tinymce_plugins[] = "/".$document_root."tinymce_plugins/image_gallery/plugin.js";
-			$tinymce_plugins[] = "/".$document_root."tinymce_plugins/video/plugin.js";
-			$tinymce_plugins[] = "/".$document_root."tinymce_plugins/templates/plugin.js";
+	require_once($BASE.'/config/paths.php');
