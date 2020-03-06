cf8b7262f9465aa3f511fd0cf395f8a2e6bb73cb
Sean Gillen <sean@forgetmenotshelter.org>
2020-03-05 23:26:01 -0600

    change global variables to functions

--- src/templates/logo.php
+++ src/templates/logo.php
@@ -1,5 +1,5 @@
 <?php
 require_once(__DIR__ . "/../common.php");
-function logo(): void {
-	echo "<a href=\"/\"><img src=\"$assets/logo.png\" id=\"logo\" alt=\"Forget Me Not Animal Shelter\"></a>";
-}
+function logo(): void { ?>
+	<a href="/"><img src="<?=assets()?>/logo.png" id="logo" alt="Forget Me Not Animal Shelter"></a>
+<?php }
