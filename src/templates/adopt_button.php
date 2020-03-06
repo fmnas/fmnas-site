6abdafa18beb611e96ba25de4803c624d14fc29c
Sean Gillen <sean@forgetmenotshelter.org>
2020-03-06 17:58:16 -0600

    templatify adopt button

---
+++ src/templates/adopt_button.php
@@ -0,0 +1,11 @@
+<?php
+function adopt_button() { ?>
+
+<form id="adopt" action="/application" method="POST">
+	<h2>Adopt a Pet</h2>
+	<a href="/application">
+		<button id="adopt_button" type="submit">Apply Online Now</button>
+	</a>
+</form>
+
+<?php }
