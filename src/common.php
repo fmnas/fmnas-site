4b5aa67b7dca2e6b9b9a2783b8ee47d1391c2d11
Sean Gillen <sean@forgetmenotshelter.org>
2020-03-05 22:39:13 -0600

    add  and  to common.php

--- src/common.php
+++ src/common.php
@@ -1,6 +1,18 @@
 <?php
 declare(strict_types = 1);
 
+function startsWith(string $haystack, string $needle): bool {
+	return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
+}
+
+function endsWith(string $haystack, string $needle): bool {
+	return substr_compare($haystack, $needle, -strlen($needle)) === 0;
+}
+
+function contains(string $haystack, string $needle): bool {
+	return strpos($haystack, $needle) !== false;
+}
+
 /**
  * The absolute path to the site root directory (containing admin/, public/, secrets/, src/)
  */
@@ -11,9 +23,26 @@ $root = dirname(__FILE__, 1);
  */
 $src = "$root/src";
 
-var_dump(getcwd()); // TODO: turn this into the assets path
+/**
+ * The absolute path to the templates directory
+ */
+$t = "$src/templates";
 
 /**
  * The relative path to the assets directory (from the file where execution started, i.e. the current page)
  */
-$assets = "//assets";
+$assets = (function(): string {
+	$cwd = getcwd();
+	$host = $_SERVER["HTTP_HOST"];
+	$adminSubdomain = "admin.";
+	if (endsWith($cwd, "/public")) {
+		return "assets";
+	}
+	if (contains($cwd, "/public/")) {
+		return "/assets";
+	}
+	if (startsWith($host, $adminSubdomain)) {
+		return "//" . substr($host, strlen($adminSubdomain)) . "/assets";
+	}
+	return "/assets"; // give up and hope
+})();
---
+++ src/templates/donate.php
@@ -0,0 +1,3 @@
+<?php function donate(): void { ?>
+	Please donate
+<?php }
