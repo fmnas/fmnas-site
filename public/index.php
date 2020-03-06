2e5ba39bb1d5d2746e2c509f2ab47771f89b9ec7
Sean Gillen <sean@forgetmenotshelter.org>
2020-03-05 22:08:44 -0600

    add common.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-
+require_once("../src/common.php");
 ?>
 <!DOCTYPE html>
-<title>Forget Me Not Animal Shelter</title>
-<p>Hello!
+<title>Forget Me Not Animal Shelter of Ferry County</title>
+<img src="/assets/logo.png" id="logo" alt="Forget Me Not Animal Shelter">
