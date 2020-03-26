974fede2773558fc73f0eafbf7758c2bd5a3d107
Sean Gillen <sean@forgetmenotshelter.org>
2020-03-26 12:57:23 -0700

    remove extraneous prints preventing 404 on false asset

--- public/listing.php
+++ public/listing.php
@@ -3,5 +3,4 @@ require_once("../src/common.php");
 require_once("$src/pet.php");
 /* @var $path string */
 /* @var $species Species */
-var_dump($path, $species);
 // Exit afterwards if this is indeed a listing
