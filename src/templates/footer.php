c430e9463c479ce4b3e22817491ca86598fe02ad
Sean Gillen <sean@forgetmenotshelter.org>
2020-03-25 23:06:21 -0700

    remove stray > from footer

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -2,10 +2,10 @@
 require_once(__DIR__ . "/../common.php");
 function footer(): void { ?>
 <footer>
-	<address><?=nl2br(_G_address())?>></address>
+	<address><?=nl2br(_G_address())?></address>
 	<p class="hours">Open hours Wednesday and Saturday 1:00 &ndash; 3:00; all other days by appointment</p>
 	<p><a href="/application">Send in an application</a> to become a pre-approved adopter; we can schedule an appointment for you to meet all the pets that interest you at the shelter, or send the pet of your dreams out to you on one of our regular transports!
-	<p class="big">For more information, call <a href="tel:<?=_G_phone_intl()?>"><?=_G_phone()?>></a><br>
+	<p class="big">For more information, call <a href="tel:<?=_G_phone_intl()?>"><?=_G_phone()?></a><br>
 		fax <?=_G_fax()?><br>
 		or <a data-email>send email</a>
 </footer>
