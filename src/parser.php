20007c55b26a81bc07c74f06378f0e9f37f1fe0b
Sean Gillen <sean@forgetmenotshelter.org>
2020-04-04 10:02:26 -0500

    first attempt at parsing

--- src/parser.php
+++ src/parser.php
@@ -1,10 +1,30 @@
 <?php
+require_once __DIR__ . "/common.php";
+require_once "$src/dependencies.php";
+Dependencies::parsedown();
+Dependencies::lightncandy();
+
+
 /**
  * Parse with Handlebars and Parsedown
  * @param string $raw Handlebars markdown
  * @return string HTML code
  */
 function parse(string $raw): string {
-	// TODO
-	return $raw;
+	$Parsedown = new Parsedown();
+	return $Parsedown->text(
+		LightnCandy::prepare(
+			LightnCandy::compile($raw, [
+				"partialresolver" => function($cx, $name) {
+					$TEMPLATE_DIR = root() . "/admin/templates";
+					if (file_exists("$TEMPLATE_DIR/$name")) {
+						return file_get_contents("$TEMPLATE_DIR/$name");
+					} else {
+						log_err("Partial $name not found");
+						return "<!-- ERROR: Handlebars partial $name not found! -->";
+					}
+				}
+			])
+		)()
+	);
 }
