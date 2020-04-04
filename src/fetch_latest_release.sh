052192a9f5176a0ee5ab2c7feab42ded68e42f33
Sean Gillen <sean@forgetmenotshelter.org>
2020-04-04 10:13:17 -0500

    Verbose fetcher

--- src/fetch_latest_release.sh
+++ src/fetch_latest_release.sh
@@ -8,16 +8,23 @@ if [ -z "$tarball" ]; then
   echo >&2 "Failed to fetch latest release of $1/$2 from github."
   exit 2
 fi
+echo "Tarball URL is $tarball";
 if [ -e "$2/VERSION" ]; then
   existing="$(cat "$2/VERSION")"
   if [ "$tarball" = "$existing" ]; then
-    echo "Latest version already downloaded."
+    echo "Latest version already downloaded. Exiting."
     exit 0
   fi
 fi
 rm -r "${2:?}/" 2>/dev/null
+echo "Downloading tarball..."
 curl -sLo "$2.tgz" "$tarball" &&
+  echo "Making directory..." &&
   mkdir "$2" &&
+  echo "Extracting tarball..." &&
   tar -xzf "$2.tgz" -C "$2" --strip-components=1 &&
+  echo "Removing downloaded tarball..." &&
   rm "$2.tgz" &&
-  echo "$tarball" >"$2/VERSION"
+  echo "Saving version number..." &&
+  echo "$tarball" >"$2/VERSION" &&
+  echo "Successfully fetched latest version of $1/$2"
