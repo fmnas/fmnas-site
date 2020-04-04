b85c3b9da5038eeca50bb9a5b3348bf45508f66d
Sean Gillen <sean@forgetmenotshelter.org>
2020-04-04 08:01:09 -0500

    Listing page parity with old site; get generated values from db

--- src/errors/404.php
+++ src/errors/404.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . "/../common.php";
 header("HTTP/1.0 404 Not Found");
 ?>
 <!DOCTYPE html>
@@ -8,5 +7,6 @@ header("HTTP/1.0 404 Not Found");
 <p>:(
 <!-- TODO: 404 page -->
 <?php
+include_once __DIR__ . "/../common.php";
 log_err("404 error at path " . $path ?? $_SERVER["REQUEST_URI"]);
 exit(404);
