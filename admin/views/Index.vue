428409180362cca6dfacb8e96938de675bb61bc0
Sean Gillen <sean@forgetmenotshelter.org>
2021-04-27 22:24:55 -0500

    Use Vue Router

---
+++ admin/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <listings :species="$route.params.species"/>
+    <transport-date/>
+</template>
+
+<script>
+  import TransportDate from '../components/TransportDate.vue';
+  import Listings from '../components/Listings.vue';
+
+  export default {
+    name: 'Index',
+    components: {Listings, TransportDate},
+    created: () => {
+      console.log('created Index')
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin/views/Listing.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Species is: {{ $route.params.species }}</p>
+    <p>Pet is: {{ $route.params.pet }}</p>
+</template>
+
+<script>
+  export default {
+    name: 'Listing',
+  };
+</script>
+
+<style scoped>
+
+</style>
