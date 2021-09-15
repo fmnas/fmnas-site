86ac082a841eee607df69cbce442bccd8596e6af
Sean Gillen <sean@forgetmenotshelter.org>
2021-09-14 18:17:59 -0700

    add species header to listings template

--- admin/components/Listings.vue
+++ admin/components/Listings.vue
@@ -1,34 +1,35 @@
 <template>
-    <p v-for="listing of listings">{{ listing }}</p>
+  <h1>Adoptable {{ species || 'pets' }}</h1>
+  <p v-for="listing of listings">{{ listing }}</p>
 </template>
 
 <script>
-  export default {
-    name: 'Listings',
-    props: ['species'],
-    data() {
-      return {
-        api_url: '/api/listings',
-        listings: [],
-      };
-    },
-    created() {
-      if (this.species) {
-        this.api_url += `/?species=${this.species}`;
-      }
-    },
-    mounted() {
-      // @todo Add a loading indicator for listings
-      fetch(this.api_url, {
-        method: 'get',
-      }).then(res => {
-        if (!res.ok) throw res;
-        return res.json();
-      }).then(data => {
-        this.listings = data;
-      });
-    },
-  };
+export default {
+  name: 'Listings',
+  props: ['species'],
+  data() {
+    return {
+      api_url: '/api/listings',
+      listings: [],
+    };
+  },
+  created() {
+    if (this.species) {
+      this.api_url += `/?species=${this.species}`;
+    }
+  },
+  mounted() {
+    // @todo Add a loading indicator for listings
+    fetch(this.api_url, {
+      method: 'get',
+    }).then(res => {
+      if (!res.ok) throw res;
+      return res.json();
+    }).then(data => {
+      this.listings = data;
+    });
+  },
+};
 </script>
 
 <style scoped>
