2b619cb3de27f64fb409c8816f5fad3c709854cd
Sean Gillen <sean@forgetmenotshelter.org>
2021-09-14 18:16:55 -0700

    show transport date editor only when editing homepage

--- admin/views/Index.vue
+++ admin/views/Index.vue
@@ -1,16 +1,16 @@
 <template>
-    <listings :species="$route.params.species"/>
-    <transport-date/>
+  <transport-date v-if="!$route.params.species"/>
+  <listings :species="$route.params.species"/>
 </template>
 
 <script>
-  import TransportDate from '../components/TransportDate.vue';
-  import Listings from '../components/Listings.vue';
+import TransportDate from '../components/TransportDate.vue';
+import Listings from '../components/Listings.vue';
 
-  export default {
-    name: 'Index',
-    components: {Listings, TransportDate},
-  };
+export default {
+  name: 'Index',
+  components: {Listings, TransportDate},
+};
 </script>
 
 <style scoped>
