63df37f49a079029f8c9b29e01fb31bb56631274
Sean Gillen <sean@forgetmenotshelter.org>
2021-09-16 18:12:54 -0700

    make config available globally

--- admin/common.js
+++ admin/common.js
@@ -2,11 +2,25 @@ function r404(path) {
   window.location.href = `/404.php?p=${encodeURIComponent(path)}`;
 }
 
-const getPathForPet = (pet, config) =>
-  `${config['species']?.[pet['species']]?.['plural']}/${pet['id']}${pet['name']?.split(' ').join('')}`;
+const globalsPromise = fetch('/api/config', {
+  method: 'get',
+}).then(res => {
+  if (!res.ok) throw res;
+  return res.json();
+}).then((config) => {
+  return {
+    methods: {
+      getPathForPet: (pet) =>
+        `${config['species']?.[pet['species']]?.['plural']}/${pet['id']}${pet['name']?.split(' ').join('')}`,
+    },
+    data() {
+      return {
+        config,
+      };
+    },
+  };
+});
 
-const globals = {
-  methods: {
-    getPathForPet,
-  },
-};
+// Dummy definitions for PhpStorm:
+const config = {};
+const getPathForPet = (pet) => {};
--- admin/views/Listing.vue
+++ admin/views/Listing.vue
@@ -1,12 +1,12 @@
 <template>
-    <p>Species is: {{ $route.params.species }}</p>
-    <p>Pet is: {{ $route.params.pet }}</p>
+  <p>Species is: {{ $route.params.species }}</p>
+  <p>Pet is: {{ $route.params.pet }}</p>
 </template>
 
 <script>
-  export default {
-    name: 'Listing',
-  };
+export default {
+  name: 'Listing',
+};
 </script>
 
 <style scoped>
