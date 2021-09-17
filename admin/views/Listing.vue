6f768d6b42d4f90045e5a62de8fed3e041c3c99d
Sean Gillen <sean@forgetmenotshelter.org>
2021-09-16 21:05:04 -0700

    Migrate PHP listing editor to Vue

--- admin/views/Listing.vue
+++ admin/views/Listing.vue
@@ -1,7 +1,42 @@
 <template>
-  <p>Species is: {{ species }}</p>
-  <p>Pet is: {{ path }}</p>
-  {{ JSON.stringify(pet) }}
+  <form method="POST">
+    <ul>
+      <li class="name">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" v-model="pet['name']" required>
+      <li class="species">
+        <label for="species">Species</label>
+        <select name="species" id="species" v-model="pet['species']" required>
+          <option value=""></option>
+          <option v-for="s of config['species']" :value="s['id']">{{ s['name'] }}</option>
+        </select>
+      <li class="breed">
+        <label for="breed">Breed/info</label>
+        <input type="text" name="breed" id="breed" v-model="pet['breed']">
+      <li class="dob">
+        <label for="dob"><abbr title="date of birth">DOB</abbr></label>
+        <input type="date" name="dob" id="dob" v-model="pet['dob']" required>
+      <li class="sex">
+        <fieldset>
+          <legend>Sex</legend>
+          <label v-for="sex of config['sexes']">
+            <input type="radio" name="sex" :value="sex['key']" v-model="pet['sex']" required>
+            <abbr :title="sex['name']">{{ sex['name'][0] }}</abbr>
+          </label>
+        </fieldset>
+      <li class="fee">
+        <label for="fee">Fee</label>
+        <input type="text" name="fee" id="fee" v-model="pet['fee']">
+      <li class="status">
+        <label for="status">Status</label>
+        <select name="status" id="status" v-model="pet['status']" required>
+          <option value=""></option>
+          <option v-for="status of config['statuses']" :value="status['key']">
+            {{ status['name'] }}
+          </option>
+        </select>
+    </ul>
+  </form>
 </template>
 
 <script>
