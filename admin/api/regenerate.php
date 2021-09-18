121c00b728909525ab3301f749e74048cb4e417e
Sean Gillen <sean@forgetmenotshelter.org>
2021-09-18 10:45:51 -0700

    regenerate api

---
+++ admin/api/regenerate.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'api.php';
+require_once "$src/generator.php";
+
+endpoint(...[
+    'get'          => function() use ($db): Result {
+        generate();
+        return new Result(200);
+    },
+    'get_value' => $reject,
+    'post' => $reject,
+    'post_value' => $reject,
+    'delete' => $reject,
+    'delete_value' => $reject,
+]);
