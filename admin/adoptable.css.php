6c39abdb77276c43a23ca4d76e30e9829aeee862
Sean Gillen <sean@forgetmenotshelter.org>
2021-09-17 19:46:20 -0700

    Preview in listing editor

---
+++ admin/adoptable.css.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type: text/css');
+readfile("../public/adoptable.css");
+readfile("../public/adoptable.generated.css");
--- admin/common.js
+++ admin/common.js
@@ -2,6 +2,12 @@ function r404(path) {
   window.location.href = `/404.php?p=${encodeURIComponent(path)}`;
 }
 
+const ucfirst = (str = '') => str.charAt(0).toUpperCase() + str.slice(1);
+
+// Dummy definitions for PhpStorm
+const config = {};
+const petAge = (pet) => '';
+
 const globalsPromise = fetch('/api/config', {
   method: 'GET',
 }).then(res => {
@@ -10,10 +16,13 @@ const globalsPromise = fetch('/api/config', {
 }).then((config) => {
   const getPathForPet = (pet) => `${pet['id']}${pet['name']?.split(' ').join('')}`;
   const getFullPathForPet = (pet) => `${config['species']?.[pet['species']]?.['plural']}/${getPathForPet(pet)}`;
+  const petAge = (pet) => 'pet age needs implementation';
   return {
     methods: {
+      ucfirst,
       getPathForPet,
       getFullPathForPet,
+      petAge,
     },
     data() {
       return {
