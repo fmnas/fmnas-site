33bdb8f7a7fa008934ba47e1f81f93e6eab98993
Sean Gillen <sean@forgetmenotshelter.org>
2021-09-17 17:11:12 -0700

    Don't count adopted pets when counting adoptable pets

--- src/db.php
+++ src/db.php
@@ -215,6 +215,7 @@ class Database {
         if (!($getAllSpecies = $this->db->prepare("
 			SELECT species.*, COUNT(pets.id) AS species_count
 			FROM species LEFT JOIN pets ON species.id = pets.species
+			JOIN statuses on pets.status = statuses.id AND statuses.listed = 1
 			GROUP BY species.id
 			"))) {
             log_err("Failed to prepare getAllSpecies: {$this->db->error}");
