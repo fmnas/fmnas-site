9e4fcb9488d5f7bc329d2c504fdc19eba0384b9d
Sean Gillen <sean@forgetmenotshelter.org>
2022-01-16 15:50:26 -0800

    remove deprecated requires for html5 parser

--- src/form.php
+++ src/form.php
@@ -99,8 +99,6 @@ require_once("$html5_php_path/src/HTML5/Exception.php");
 require_once("$html5_php_path/src/HTML5/InstructionProcessor.php");
 require_once("$html5_php_path/src/HTML5/Parser/EventHandler.php");
 require_once("$html5_php_path/src/HTML5/Serializer/RulesInterface.php");
-require_once("$html5_php_path/src/HTML5/Parser/InputStream.php");
-require_once("$html5_php_path/src/HTML5/Parser/StringInputStream.php");
 require_once("$html5_php_path/src/HTML5/Parser/Scanner.php");
 require_once("$html5_php_path/src/HTML5/Parser/Tokenizer.php");
 require_once("$html5_php_path/src/HTML5/Parser/TreeBuildingRules.php");
@@ -109,7 +107,6 @@ require_once("$html5_php_path/src/HTML5/Serializer/HTML5Entities.php");
 require_once("$html5_php_path/src/HTML5/Serializer/OutputRules.php");
 require_once("$html5_php_path/src/HTML5/Serializer/Traverser.php");
 require_once("$html5_php_path/src/HTML5/Parser/DOMTreeBuilder.php");
-require_once("$html5_php_path/src/HTML5/Parser/FileInputStream.php");
 require_once("$html5_php_path/src/HTML5/Parser/ParseError.php");
 require_once("$html5_php_path/src/HTML5/Parser/CharacterReference.php");
 
