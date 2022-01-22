c712403c0c59244db21362538e40cda343b92fdd
Sean Gillen <sean@forgetmenotshelter.org>
2022-01-22 11:23:34 -0800

    Checkout config_sample.php from test

---
+++ secrets/config_sample.php
@@ -0,0 +1,16 @@
+<?php
+
+class Config {
+	static string $db_name = "shelter";
+	static string $db_username = "username";
+	static string $db_pass = "password";
+	static string $db_host = "localhost";
+	static string $phpmailer_path = "/home/php/PHPMailer";
+	static string $html5_php_path = "/home/php/html5-php";
+	static string $smtp_host = "smtp.gmail.com";
+	static bool $smtp_auth = true;
+	static string $smtp_security = "tls"; // valid values are "", "tls", "ssl"
+	static int $smtp_port = 587;
+	static string $smtp_username = "example@gmail.com";
+	static string $smtp_password = "password";
+}
