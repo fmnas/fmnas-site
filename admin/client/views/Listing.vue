fdcddd4dc6fae038819a5f5edb872a9939667123
TODO <todo-actions[bot]@users.noreply.github.com>
2022-01-29 04:48:21 +0000

    Update TODO references: #135, #136, #137, #138, #139, #140, #141

--- admin/client/views/Listing.vue
+++ admin/client/views/Listing.vue
@@ -164,7 +164,7 @@ Introducing {{name}} <` + /* i hate javascript */ `!-- Write the rest of the lis
 			});
 		} else {
 			// Creating a new listing
-			// TODO [$61f4c71115395d0009dba036]: Type for species.
+			// TODO [#140]: Type for species.
 			this.pet['species'] =
 				(Object.values(store.state.config['species']) as any).find((s: any) => s['plural'] === this.species)?.['id'];
 			this.originalDescription = this.description;
@@ -237,7 +237,7 @@ Introducing {{name}} <` + /* i hate javascript */ `!-- Write the rest of the lis
 			alert('Should bring up the profile image editor.');
 			// TODO [#60]: profile image editor
 		},
-		// TODO [$61f4c71115395d0009dba037]: Type for sex
+		// TODO [#141]: Type for sex
 		sexClick(sex: any) {
 			// Allow deselecting a sex rather than just selecting one.
 			this.pet['sex'] = this.pet['sex'] === sex['key'] ? null : sex['key'];
