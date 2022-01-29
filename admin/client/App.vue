2ac54ab33f567c683ec5fcc311ec6dc0f263b102
Sean Gillen <sean@forgetmenotshelter.org>
2022-01-28 20:42:35 -0800

    Use Vite to build vue site

--- admin/client/App.vue
+++ admin/client/App.vue
@@ -1,33 +1,27 @@
 <template>
-  <div id="nav">
+  <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+    <router-link to="/cats">Cats</router-link> |
+	  <router-link to="/dogs">Dogs</router-link>
+  </nav>
   <router-view/>
 </template>
 
 <script>
-import {getConfig} from './common';
-
 export default {
 	name: 'FMNAS Admin',
-	async mounted() {
-		const config = await getConfig();
-		this.$store.commit('setConfig', config);
-	}
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+	font-size: 12pt;
+	font-family: Arial, sans-serif;
 }
 
-#nav {
+nav {
+	// TODO: Redesign the admin site nav.
   padding: 30px;
 
   a {
