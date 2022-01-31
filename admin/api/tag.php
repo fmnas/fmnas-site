411c0d616ae17fe5e200822c6f23400f8a593859
Sean Gillen <sean@forgetmenotshelter.org>
2022-01-30 18:03:56 -0800

    resize image on upload

---
+++ admin/api/tag.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'api.php';
+
+// Get an imgTag. Useful to preload the cache.
+endpoint(...[
+		'get' => $reject,
+		'get_value' => function($key) use ($db): Result {
+			if (!is_numeric($key)) {
+				return new Result(400, error: "Asset key $key must be numeric");
+			}
+			$asset = $db->getAssetByKey(intval($key));
+			if ($asset === null) {
+				return new Result(404, error: "Asset $key not found");
+			}
+			return new Result(200, $asset->imgTag());
+		},
+		'put' => $reject,
+		'put_value' => $reject,
+		'post' => $reject,
+		'post_value' => $reject,
+		'delete' => $reject,
+		'delete_value' => $reject,
+]);
