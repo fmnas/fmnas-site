a34364175d5b38e4d7bda61e4f63fb1235fbe63f
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-04 13:45:10 -0800

    Fix some console errors

--- admin/client/components/Photos.vue
+++ admin/client/components/Photos.vue
@@ -65,10 +65,13 @@ export default defineComponent({
 	emits: ['update:modelValue', 'update:promises'],
 	computed: {
 		photos: {
-			get(): Asset[] | undefined {
+			get(): Asset[] {
+				if (!this.modelValue?.[0]) {
+					return [];
+				}
 				return this.modelValue;
 			},
-			set(value: Asset[] | undefined): void {
+			set(value: Asset[]): void {
 				this.$emit('update:modelValue', value);
 			},
 		},
