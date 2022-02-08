cdc11ebe338dbd0c86fe4c3e92d7d331a5884b09
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-08 13:09:41 -0800

    responsive header

--- src/templates/header.php
+++ src/templates/header.php
@@ -16,6 +16,7 @@ function pageHeader(): void { ?>
 					<li><a href="https://www.facebook.com/ForgetMeNotAnimalShelter/">Facebook</a>
 					<li><a href="/blog">Blog</a>
 				</ul>
+				<a class="printonly url" href="/">https://<?=_G_public_domain()?></a>
 			</div>
 			<form class="adopt" action="/application" method="POST">
 				<h2><a href="/application">Adopt a Pet</a></h2>
