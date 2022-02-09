e2c7be1cd03fd9a46d26dab28717a77bcaa86140
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-08 17:54:02 -0800

    form 990 button

---
+++ public/f990.ts
@@ -0,0 +1,18 @@
+const div: HTMLDivElement = document.querySelector('div.f990')!;
+const button: HTMLButtonElement = document.createElement('button');
+const select: HTMLSelectElement = document.createElement('select');
+div.querySelectorAll('a').forEach((a => {
+	const option: HTMLOptionElement = document.createElement('option');
+	option.value = a.href;
+	option.innerText = a.innerText;
+	select.appendChild(option);
+}));
+button.append(document.createTextNode('View our '), select, document.createTextNode(' IRS Form 990'));
+select.addEventListener('click', (e) => {
+	e.stopImmediatePropagation();
+});
+button.addEventListener('click', () => {
+	window.location.href = select.value;
+});
+button.classList.add('f990');
+div.replaceWith(button);
