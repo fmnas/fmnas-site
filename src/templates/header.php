23ef52ddf1f1c5e3309822c05c02a382b670d499
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-09 21:21:03 -0800

    use local paypal assets

--- src/templates/header.php
+++ src/templates/header.php
@@ -26,9 +26,8 @@ function pageHeader(): void { ?>
 				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" class="paypal">
 					<input type="hidden" name="cmd" value="_s-xclick">
 					<input type="hidden" name="hosted_button_id" value="9649881">
-					<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit"
+					<input type="image" src="/assets/btn_donateCC_LG.gif" name="submit"
 							alt="Donate through PayPal">
-					<img src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 				</form>
 				<a href="https://www.networkforgood.org/donation/ExpressDonation.aspx?ORGID2=91-1996344" class="nfg darken"><img
 							src="<?=assets()?>/networkforgoodlogo.gif" alt="Donate through Network For Good"></a>
