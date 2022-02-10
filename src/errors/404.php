f3b3f246341295cd63c79b718196d0e5e338bfa7
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-09 20:15:09 -0800

    404 page
    Fixes #18

--- src/errors/404.php
+++ src/errors/404.php
@@ -3,9 +3,18 @@ header("HTTP/1.0 404 Not Found");
 ?>
 	<!DOCTYPE html>
 	<title>404 Not Found</title>
-	<h1>404 Not Found</h1>
-	<p>:(
-		<!-- TODO [#18]: 404 page -->
+	<style>
+		html {
+			display: flex;
+			width: 100%;
+			height: 100%;
+			box-sizing: border-box;
+			margin: 0;
+			justify-content: center;
+			align-items: center;
+		}
+	</style>
+	<img src="//http.cat/404" alt="404 Not Found">
 <?php
 include_once __DIR__ . "/../common.php";
 log_err("404 error at path " . $path ?? $_SERVER["REQUEST_URI"]);
