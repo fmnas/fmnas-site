f839d79978ae62061dc1ee9461437b3cdcc8e44c
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-14 00:06:13 -0800

    Write image-resize function

--- functions/resize-image.go
+++ functions/resize-image.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
 func ResizeImage(w http.ResponseWriter, r *http.Request) {
-	const maxMemory = 20 * 1024 * 1024
-	if err := r.ParseMultipartForm(maxMemory); err != nil {
+	if err := r.ParseMultipartForm(20 << 20); err != nil {
 		http.Error(w, "Unable to parse request", http.StatusBadRequest)
-		log.Printf("Error parsing form: %v", err)
+		log.Printf("Error parsing request: %v", err)
 		return
 	}
 
@@ -21,11 +23,69 @@ func ResizeImage(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 
-	for _, headers := range r.MultipartForm.File {
-		for _, h := range headers {
-			fmt.Fprintf(w, "File uploaded: %q (%v bytes)", h.Filename, h.Size)
-			// Use h.Open() to read the contents of the file.
-		}
+	f, fh, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Error extracting image from request", http.StatusBadRequest)
+		log.Printf("Error extracting image from request: %v", err)
+		return
 	}
 
+	s := int(fh.Size)
+	b := make([]byte, s)
+	n, err := f.Read(b)
+	if err != nil {
+		http.Error(w, "Error reading image bytes", http.StatusInternalServerError)
+		log.Printf("Error reading image bytes: %v", err)
+		return
+	}
+	if n != s {
+		http.Error(w, fmt.Sprintf("Read %v bytes, expected %v", n, s), http.StatusBadRequest)
+		log.Printf("Read %v bytes, expected %v", n, s)
+		return
+	}
+
+	h1 := r.PostFormValue("height")
+	h2, err := strconv.Atoi(h1)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error parsing supplied height %v", h1), http.StatusBadRequest)
+		log.Printf("Error parsing supplied height %v", h1)
+		return
+	}
+	nh := uint(h2)
+
+	imagick.Initialize()
+	defer imagick.Terminate()
+	omw := imagick.NewMagickWand()
+	if err := omw.ReadImageBlob(b); err != nil {
+		http.Error(w, "Error reading image", http.StatusBadRequest)
+		log.Printf("Error reading image: %v", err)
+	}
+
+	mw := omw.Clone()
+	ow := mw.GetImageWidth()
+	oh := mw.GetImageHeight()
+	nw := ow * nh / oh
+
+	if err := mw.ResizeImage(nw, nh, imagick.FILTER_LANCZOS, 1); err != nil {
+		http.Error(w, "Error resizing image", http.StatusInternalServerError)
+		log.Printf("Error resizing image: %v", err)
+	}
+
+	if err := mw.SetImageCompression(imagick.COMPRESSION_JPEG); err != nil {
+		http.Error(w, "Error setting compression type", http.StatusInternalServerError)
+		log.Printf("Error setting compression type: %v", err)
+	}
+
+	if err := mw.SetImageCompressionQuality(80); err != nil {
+		http.Error(w, "Error setting compression quality", http.StatusInternalServerError)
+		log.Printf("Error setting compression quality: %v", err)
+	}
+
+	out := mw.GetImageBlob()
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	if _, err := w.Write(out); err != nil {
+		http.Error(w, "Error writing response", http.StatusInternalServerError)
+		log.Printf("Error writing response: %v", err)
+	}
 }
