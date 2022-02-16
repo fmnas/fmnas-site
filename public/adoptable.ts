884274ee487fe455b72d19a1b66f2747b1e13948
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-16 13:41:24 -0800

    Redo status tooltips.
    Fixes #16

--- public/adoptable.ts
+++ public/adoptable.ts
@@ -1,4 +1,5 @@
 const MAX_CONDENSE = .75;
+const CUTOFF_WIDTH = 450; // sync with $cutoff in adoptable.scss
 const tbody: HTMLTableSectionElement = document.querySelector('table.listings tbody')!;
 const lastRow: HTMLTableSectionElement = document.querySelector('table.listings.last-row tbody')!;
 
@@ -14,15 +15,83 @@ function addEventListeners(link: Element) {
 	listing.addEventListener('pointerup', () => listing.classList.remove('active'));
 	listing.addEventListener('pointerout', () => listing.classList.remove('active'));
 	listing.querySelector('td.inquiry')?.addEventListener('click', (e) => e.stopPropagation());
+	listing.querySelector('td.inquiry')?.addEventListener('pointerdown', (e) => e.stopPropagation());
 	listing.querySelector('td.fee')?.addEventListener('click', (e) => e.stopPropagation());
+	listing.querySelector('td.fee')?.addEventListener('pointerdown', (e) => e.stopPropagation());
+}
+
+function hashChangeHandler() {
+	document.querySelectorAll('section.explanations > aside > div.close')
+		.forEach((closeButton) => {
+			const button = closeButton as HTMLDivElement;
+			const aside = closeButton.closest('aside')!;
+			const statusClass: string = [...aside.classList].find((className) => className.startsWith('st_'))!;
+			if (!window.location.hash.endsWith(statusClass)) {
+				aside.classList.remove('shown');
+				button.replaceWith();
+			}
+		});
+}
+
+function clearHash() {
+	history.replaceState('', document.title, window.location.pathname + window.location.search);
+}
+
+window.addEventListener('hashchange', hashChangeHandler);
+window.addEventListener('load', clearHash);
+
+function showMobileTooltip(e: Event) {
+	clearHash();
+	const feeText = e.target as HTMLSpanElement;
+	const row = feeText.closest('tr')!;
+	const statusClass: string = [...row.classList].find((className) => className.startsWith('st_'))!;
+	const explanation: HTMLElement = document.querySelector(`section.explanations > aside.${statusClass}`)!;
+	const closeButton = document.createElement('div');
+	closeButton.classList.add('close');
+	closeButton.innerHTML = '&#xe5c9';
+	explanation.classList.add('shown');
+	explanation.appendChild(closeButton);
+	closeButton.addEventListener('click', hideMobileTooltip);
+	window.location.hash = `#${statusClass}`;
+	e.stopPropagation();
+}
+
+function hideMobileTooltip(e: Event) {
+	const closeButton = e.target as HTMLElement;
+	closeButton.closest('aside')!.classList.remove('shown');
+	closeButton.replaceWith();
+	if (window.location.hash) {
+		history.back(); // clear the hash state and trigger hashchangehandler
+	}
+	e.stopPropagation();
+}
+
+function setupMobileTooltips() {
+	document.querySelectorAll('tr.explain span.fee').forEach((feeText) => {
+		const span = feeText as HTMLSpanElement;
+		span.addEventListener('click', showMobileTooltip);
+	});
+}
+
+function setupDesktopTooltips() {
+	document.querySelectorAll('tr.explain span.fee').forEach((feeText) => {
+		const span = feeText as HTMLSpanElement;
+		span.addEventListener('click', showMobileTooltip);
+	});
+	document.querySelectorAll('section.explanations > aside').forEach((asideEl) => {
+		const aside = asideEl as HTMLElement;
+		aside.click();
+	});
 }
 
 function resizer() {
+	// Scale data to fit the grid column.
 	lastRow.replaceChildren();
 	tbody.querySelectorAll('tr').forEach((listing: HTMLTableRowElement) => {
 		listing.classList.remove('yote');
 		const referenceRow: HTMLTableCellElement = listing.querySelector('td.img')!;
 		const referenceWidth: number = referenceRow.clientWidth;
+		listing.querySelector('aside.explanation')?.classList.add('hidden');
 		listing.querySelectorAll('td, th').forEach((row: Element) => {
 			if (row === referenceRow) {
 				return;
@@ -40,8 +109,10 @@ function resizer() {
 			}
 			cell.style.setProperty('overflow-x', 'hidden');
 		});
+		listing.querySelector('aside.explanation')?.classList.remove('hidden');
 	});
 
+	// Move the last grid row into a separate, centered grid.
 	const gridColumns = window.getComputedStyle(tbody).getPropertyValue('grid-template-columns').split(' ').length;
 	let listings = [...tbody.querySelectorAll('tr')];
 	let totalSize = 0;
@@ -81,10 +152,15 @@ function resizer() {
 		}
 	}
 	lastRow.querySelectorAll('th.name a[href]').forEach(addEventListeners);
+
+	// Set up the event listeners for explanatory tooltips.
+	window.matchMedia(`(max-width: ${CUTOFF_WIDTH - 1}px)`).matches ? setupMobileTooltips() : setupDesktopTooltips();
 }
+
 // TODO [#286]: Resizer miscalculates width when resizing after initial load in Firefox.
 
 tbody.querySelectorAll('th.name a[href]').forEach(addEventListeners);
 
 resizer();
 window.addEventListener('resize', resizer);
+
