2ecc80fc6712ce2ea5842576e5211ee9198c4b6b
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-17 14:09:52 -0800

    Use combined id in email link

--- public/listing.php
+++ public/listing.php
@@ -64,7 +64,7 @@ if (!($pet = $db->getPetByPath($path))) {
 		?>
 		<form action="/application" method="GET" id="bottom_form">
 			If you would like to know more about, or think you might like to adopt, <?=htmlspecialchars($pet->name())?>,
-			<button>Apply Online Now</button> or email us at: <a data-email="adopt+<?=$pet->id?>">Adopt <?=htmlspecialchars($pet->name())?>!</a>
+			<button>Apply Online Now</button> or email us at: <a data-email="adopt+<?=$pet->id()?>">Adopt <?=htmlspecialchars($pet->name())?>!</a>
 			<input type="hidden" name="pet" value="<?=htmlspecialchars($pet->id)?>" id="hidden_id">
 		</form>
 		</section>
