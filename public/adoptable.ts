448fe2384eaf4571a89441fb9200fdc0654522a1
TODO <todo-actions[bot]@users.noreply.github.com>
2022-02-18 20:54:10 +0000

    Collect TODO comments

--- public/adoptable.ts
+++ public/adoptable.ts
@@ -203,7 +203,7 @@ function resizer(useReference: boolean = true) {
 				}
 				if (listing.classList.contains('pair') && (lastRowCount - yote === 1)) {
 					// Don't yeet a pair if we only want 1 additional column.
-					// TODO: Yeet pairs in each order first.
+					// TODO [$621007726ec75c0008872d46]: Yeet pairs in each order first.
 					continue;
 				}
 				yote += listing.classList.contains('pair') ? 2 : 1;
