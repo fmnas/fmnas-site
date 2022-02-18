3ba4bb3a2e18d7776c56380d70ce84b2b74af10f
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-17 19:09:10 -0800

    bump max_condense to .8

--- public/adoptable.ts
+++ public/adoptable.ts
@@ -1,4 +1,4 @@
-const MAX_CONDENSE = .75;
+const MAX_CONDENSE = .8;
 const CUTOFF_WIDTH = 450; // sync with $cutoff in adoptable.scss
 const tbody: HTMLTableSectionElement = document.querySelector('table.listings tbody')!;
 const lastRow: HTMLTableSectionElement = document.querySelector('table.listings.last-row tbody')!;
