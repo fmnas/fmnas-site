e2d64efa33e46b2d6db2c5843c6cbb46c1d81664
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-18 10:50:07 -0800

    vertical-align for divs in pet dropdown

--- admin/client/components/PetDropdownEntry.vue
+++ admin/client/components/PetDropdownEntry.vue
@@ -64,6 +64,7 @@ export default defineComponent({
       height: 100%;
       box-sizing: border-box;
       padding: $padding;
+      vertical-align: middle;
 
       &.img {
         width: $image-width;
