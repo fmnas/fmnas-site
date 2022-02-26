2d44f00d790c69c761af704a6c8d7568a3103441
Sean Gillen <sean@forgetmenotshelter.org>
2022-02-25 18:44:30 -0800

    Leave a TODO for RAM usage tests

--- tests/blackbox/test/regression_test_helper.dart
+++ tests/blackbox/test/regression_test_helper.dart
@@ -24,6 +24,7 @@ import '../bin/service.dart';
 const maxFailThreshold = 2.0;
 const avgFailThreshold = 1.1;
 const ramFailThreshold = 2.0;
+// TODO: RAM usage is very inconsistent between regression test runs.
 
 // If (new value)/(old value) goes below this threshold, update the goldens.
 const maxUpdateThreshold = 0.7;
