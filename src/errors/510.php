020e92ecf15e912c5b2a75c312f9f2aa465450ae
Sean Gillen <sean@forgetmenotshelter.org>
2022-03-01 15:45:21 -0800

    510 page

--- src/errors/510.php
+++ src/errors/510.php
@@ -3,9 +3,23 @@ header("HTTP/1.0 510 Not Extended");
 ?>
 	<!DOCTYPE html>
 	<title>510 Not Extended</title>
-	<h1>510 Not Extended</h1>
-	<p>:(
-		<!-- TODO [#20]: 510 page -->
+	<style>
+		html {
+			display: flex;
+			width: 100%;
+			height: 100%;
+			box-sizing: border-box;
+			margin: 0;
+			justify-content: center;
+			align-items: center;
+			background-color: #000;
+			text-align: center;
+		}
+	</style>
+	<img src="//http.cat/510" alt="510 Not Extended">
+	<form action="/" method="POST">
+		<button type="submit">Return to the shelter homepage</button>
+	</form>
 <?php
 include_once __DIR__ . "/../common.php";
 log_err("510 error at path " . $path ?? $_SERVER["REQUEST_URI"]);
